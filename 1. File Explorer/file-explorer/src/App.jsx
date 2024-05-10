683e1f141fe020e6e8576d0f4c7f20830b55e5ff
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-05-10 22:27:42 +0530

    create file and folder for 1 level

--- 1. File Explorer/file-explorer/src/App.jsx
+++ 1. File Explorer/file-explorer/src/App.jsx
@@ -3,15 +3,22 @@ import { useState } from 'react'
 import './App.css'
 import explorer from './data/folderData';
 import Folder from './compoents/Folder';
+import useTraverseTree from './hooks/use-traverse-tree';
 
 function App() {
   const [explorerData, setExplorerData] = useState(explorer);
-  console.log(explorer);
+  // console.log(explorer);
+
+  const { insertNode } = useTraverseTree();
+
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+  }
 
   return (
     <>
     {/* <Folder /> */}
-    <Folder explorer={explorerData} />
+    <Folder handleInsertNode={handleInsertNode} explorer={explorerData} />
     </>
   )
 }
--- 1. File Explorer/file-explorer/src/compoents/Folder.jsx
+++ 1. File Explorer/file-explorer/src/compoents/Folder.jsx
@@ -1,16 +1,59 @@
 import React, { useState } from 'react'
 
-const Folder = ({explorer}) => {
+const Folder = ({handleInsertNode, explorer}) => {
   const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpand(true);
+    setShowInput({
+      visible: true,
+      isFolder
+    });
+  }
+
+
+  const onAddFolder = (e) => {
+    if(e.keyCode === 13 && e.target.value) {
+      handleInsertNode(explorer.id, e.target.value, showInput.isFolder)
+      setShowInput({...showInput, visible: false})
+    }
+  }
+
   if( explorer.isFolder) {
     return (
       <div style={{marginTop: 5}}>
         <div className='folder' onClick={() => setExpand(!expand)}>
           <span>📁 {explorer.name}</span>
+          <div>
+            <button onClick={(e)=> handleNewFolder(e, true)}>Folder +</button>
+            <button onClick={(e)=> handleNewFolder(e, false)}>File +</button>
+          </div>
         </div>
         <div style={{ display: expand ? "block" : "none", paddingLeft: "25px"}}>
+
+          {
+             showInput.visible && (
+              <div className='inputContainer'>
+                <span>{showInput.isFolder ? "📁" : "📄"}</span>
+                <input 
+                type='text'
+                onKeyDown={onAddFolder}
+                onBlur={() => setShowInput({ ...showInput, visible: false})}
+                className='inputContainer__input'
+                autoFocus
+                />
+
+              </div>
+             )
+          }
           {explorer.items.map((exp) => {
-            return <span>{exp.name}</span>
+            // return <span>{exp.name}</span>   // rendering  only 1 level
+            return <Folder explorer={exp} key={exp.id}/>
           })}
         </div>
       </div>
